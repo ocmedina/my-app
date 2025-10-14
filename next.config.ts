2b3e47072dcc478c09c19e8fbe40a93d1e708ecd
ocmedina <octaviomedina239@gmail.com>
2025-10-14 15:48:53 -0300

    feat: Update next.config.ts to configure ESLint settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Ignora errores de ESLint durante el build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Opciones adicionales que tengas
 };
 
 export default nextConfig;
